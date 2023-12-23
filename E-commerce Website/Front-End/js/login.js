a78391b339dc128114dbd81558bb23e23dc4f984
Shreejay Mane <52676510+shreejaymane15@users.noreply.github.com>
2023-12-23 15:15:21 +0530

    Login and Register Pages

---
+++ E-commerce Website/Front-End/js/login.js
@@ -0,0 +1,9 @@
+
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var emailRegex = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+var passwordRegex = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$";
+
+function login(){
+    
+}
